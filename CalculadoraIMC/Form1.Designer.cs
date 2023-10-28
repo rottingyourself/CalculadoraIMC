292e786c3d0bd67bdb62d157cf996f0e8f3cca7e
Adriel Rodrigues <adriel.grodrigues012@gmail.com>
2023-10-27 22:06:52 -0300

    Implementando interface
    Feito resultado do IMC falta implementar a classificação do resultado

--- CalculadoraIMC/Form1.Designer.cs
+++ CalculadoraIMC/Form1.Designer.cs
@@ -28,12 +28,124 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txbPeso = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblCalculadora = new System.Windows.Forms.Label();
+            this.lblPeso = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblSaude = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txbPeso
+            // 
+            this.txbPeso.Location = new System.Drawing.Point(53, 112);
+            this.txbPeso.Name = "txbPeso";
+            this.txbPeso.Size = new System.Drawing.Size(231, 23);
+            this.txbPeso.TabIndex = 0;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(53, 182);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(231, 23);
+            this.txbAltura.TabIndex = 1;
+            // 
+            // lblCalculadora
+            // 
+            this.lblCalculadora.AllowDrop = true;
+            this.lblCalculadora.AutoSize = true;
+            this.lblCalculadora.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCalculadora.Font = new System.Drawing.Font("Bahnschrift SemiBold", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblCalculadora.Location = new System.Drawing.Point(70, 19);
+            this.lblCalculadora.Name = "lblCalculadora";
+            this.lblCalculadora.Size = new System.Drawing.Size(189, 29);
+            this.lblCalculadora.TabIndex = 2;
+            this.lblCalculadora.Text = "Calculadora IMC";
+            this.lblCalculadora.Click += new System.EventHandler(this.lblCalculadora_Click);
+            // 
+            // lblPeso
+            // 
+            this.lblPeso.AutoSize = true;
+            this.lblPeso.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblPeso.Location = new System.Drawing.Point(49, 89);
+            this.lblPeso.Name = "lblPeso";
+            this.lblPeso.Size = new System.Drawing.Size(124, 19);
+            this.lblPeso.TabIndex = 3;
+            this.lblPeso.Text = "Informe seu peso (KG)";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Bahnschrift Condensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblAltura.Location = new System.Drawing.Point(49, 151);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(180, 19);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Informe sua altura (metro e cm)";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(52, 220);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(102, 27);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblResultado.Location = new System.Drawing.Point(231, 237);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(53, 25);
+            this.lblResultado.TabIndex = 6;
+            this.lblResultado.Text = "label1";
+            // 
+            // lblSaude
+            // 
+            this.lblSaude.AutoSize = true;
+            this.lblSaude.Font = new System.Drawing.Font("Bahnschrift Condensed", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.lblSaude.Location = new System.Drawing.Point(231, 275);
+            this.lblSaude.Name = "lblSaude";
+            this.lblSaude.Size = new System.Drawing.Size(53, 25);
+            this.lblSaude.TabIndex = 7;
+            this.lblSaude.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(353, 347);
+            this.Controls.Add(this.lblSaude);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblPeso);
+            this.Controls.Add(this.lblCalculadora);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbPeso);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Calculadora IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox txbPeso;
+        private TextBox txbAltura;
+        private Label lblCalculadora;
+        private Label lblPeso;
+        private Label lblAltura;
+        private Button btnCalcular;
+        private Label lblResultado;
+        private Label lblSaude;
     }
 }
--- CalculadoraIMC/Form1.cs
+++ CalculadoraIMC/Form1.cs
@@ -6,5 +6,31 @@ namespace CalculadoraIMC
         {
             InitializeComponent();
         }
+
+        private void lblCalculadora_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+
+        {   //obter os numeros do form
+            double peso, altura, resultado;
+            peso = double.Parse(txbPeso.Text);
+            altura = double.Parse(txbAltura.Text);
+
+            resultado = peso / Math.Pow(altura,2);
+
+            if (resultado > 0 )
+            {
+
+            }
+
+
+
+
+
+
+        }
     }
 }
